9df35a03d8b1b1d78e48e8959ad84f77e80d22b3
Harry Pan <pan.xingzhi@gmail.com>
2017-02-02 22:19:30 -0800

    wip

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,33 @@
-module Main where
+module Main (main) where
 
-import Lib
+import Data.Char
 
-main :: IO ()
-main = someFunc
+import GHCJS.DOM (currentDocument)
+import GHCJS.DOM.Types (HTMLInputElement(..), FromJSString, JSString, fromJSString, toJSString, unsafeCastTo)
+import GHCJS.DOM.Document (createElementUnsafe, getBody)
+import GHCJS.DOM.HTMLInputElement (getValue, setType, setValue)
+import GHCJS.DOM.Node (appendChild)
+
+main = do
+  Just doc <- currentDocument
+  Just body <- getBody doc
+
+  loanInput <- createElementUnsafe doc (Just "input") >>= unsafeCastTo HTMLInputElement
+  interestInput <- createElementUnsafe doc (Just "input") >>= unsafeCastTo HTMLInputElement
+  yearInput <- createElementUnsafe doc (Just "input") >>= unsafeCastTo HTMLInputElement
+  calcBtn <- createElementUnsafe doc (Just "input") >>= unsafeCastTo HTMLInputElement
+  setType calcBtn (toJSString "button")
+
+  appendChild body (Just loanInput)
+  appendChild body (Just interestInput)
+  appendChild body (Just yearInput)
+  appendChild body (Just calcBtn)
+
+  setValue yearInput (Just (toJSString "year"))
+
+  Just v <- getValue yearInput
+  v :: JSString
+
+  putStrLn (fromJSString v)
+
+  return ()
